c95ca07a0a57b77d697019e614e089964bd0eeaf
djokicaetfbl <djordjepalavestra@gmail.com>
2021-07-28 15:57:16 +0200

    some fixesss

--- src/app/categories/categories-routing.module.ts
+++ src/app/categories/categories-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { ArticlesComponent } from "src/articles/articles.component";
 import { AuthGuard } from "../auth/auth-guard";
 //import { CategoriesResolverService } from "./categories-resolver.service";
 import { CategoriesComponent } from "./categories.component";
@@ -9,11 +10,11 @@ const routes: Routes = [
     {
         path: '', component: CategoriesComponent, /*canActivate: [AuthGuard],*/ // i pomocu ovoga canActivate ne mozemo direkt da pristupimo preko url /recipes
         // moram da imam praznu putanju radi lazy loading-a , a ukljucili smo u app-routing.module.ts putanju /recipes
-            /*children: [
-                { path: 'new', component: CategoryEditComponent }
-            ]*/  /* sa childre da se ucita na istoj html stranici kao i father, obavezno <router-outlet> na roditeljskoj html stranici */
+            children: [
+                { path: ':categoryName', component: ArticlesComponent }
+            ]  /* sa childre da se ucita na istoj html stranici kao i father, obavezno <router-outlet> na roditeljskoj html stranici */
     },
-    { path: 'new', component: CategoryEditComponent, canActivate: [AuthGuard], },
+    //{ path: 'new', component: CategoryEditComponent, canActivate: [AuthGuard]},
     { path: 'updateCategory/:id', component: CategoryEditComponent, canActivate: [AuthGuard],
      }
 ];
