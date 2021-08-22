4703e6fbf1b97c048b15211b7e8199b91e424c90
djokicaetfbl <djordjepalavestra@gmail.com>
2021-08-22 14:23:14 +0200

    finished

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
-//import { DataStorageService } from '../shared/data-storage.service';
 import { faCoffee, faPlus, faKey ,faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-//import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
-//import { faKey} from '@fortawesome/free-solid-svg-icons';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
@@ -32,14 +29,8 @@ export class HeaderComponent implements OnInit, OnDestroy  {
     this.userSub = this.authService.user.subscribe(user => {
       this.isAuthenticated = !!user;// ili !user ? false : true; // ako nemamo objekat user , tada nismo autenitifikovani (tj user = null)
     });
-  }
 
-  /*onNewCategory() {*/
-    //console.log("THIS ROUTE (djole) : "+this.route);
-    //this.router.navigate(['categories/new'] /*, {relativeTo: this.route}*/); // ovo je relativna putanja , posto smo vec na categories/ pa sad treba da obavjestimo router o nasoj trenutnoj ruti (recipes/) to radimo
-      // sa route: ActivatedRoute kroz relativeTo :D
-  /*    this.router.navigate(['newCategory'] /*, {relativeTo: this.route}*/ /*);
-  }*/
+  }
 
   onLogout() {
     this.authService.logout();
@@ -52,10 +43,5 @@ export class HeaderComponent implements OnInit, OnDestroy  {
   goToCart(){
     this.router.navigate(['cart']); 
   }
-
-//  onNewArticle() {
-    //newArticle
- //   this.router.navigate(['newArticle'] /*, {relativeTo: this.route}*/);
-//}
 
 }
